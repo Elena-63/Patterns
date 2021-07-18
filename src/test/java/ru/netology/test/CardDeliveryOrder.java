b9d440cc374e6acbcb8e5087e41e04ab2d76a525
Elena-63 <domanova>
2021-07-18 12:04:12 +0300

    Исправлены ошибки в CardDeliveryOrder.java

--- src/test/java/ru/netology/test/CardDeliveryOrder.java
+++ src/test/java/ru/netology/test/CardDeliveryOrder.java
@@ -1,8 +1,11 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.date.DataGenerator;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
@@ -12,26 +15,31 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CardDeliveryOrder {
-
     String name = DataGenerator.generateName();
     String phone = DataGenerator.generatePhone();
     String city = DataGenerator.generateCity();
+    String cityEng = DataGenerator.generateCityENG();
+    String nameEng = DataGenerator.generateNameEng();
+
+    @BeforeEach
+            void seTup(){
+        open("http://localhost:9999");
+    }
 
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue(city);
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
-        form.$("[data-test-id=name] input").setValue(name);
-        form.$("[data-test-id=phone] input").setValue(phone);
-        form.$("[data-test-id=agreement]").click();
-        form.$(".button").click();
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(name);
+        $("[data-test-id=phone] input").setValue(phone);
+        $("[data-test-id=agreement]").click();
+        $(".button").click();
         $(".notification_status_ok").shouldBe(visible);
         $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(3)));
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
-        form.$(".button").click();
-        $("[data-test-id=replan-notification]").waitUntil(visible, 15000);
+        $("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(7));
+        $(".button").click();
+        $("[data-test-id=replan-notification]").shouldBe(visible, Duration.ofSeconds(12));
         $(withText("Перепланировать")).click();
         $(".notification_status_ok").shouldBe(visible);
         $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.generateDate(7)));
@@ -39,11 +47,10 @@ class CardDeliveryOrder {
 
     @Test
     void shouldEnterAnIncorrectFirstAndLastName() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue(city);
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
-        $("[data-test-id=name] input").setValue("Ivanov Ivan");
+        $("[data-test-id='city'] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
+        $("[data-test-id=name] input").setValue(nameEng);
         $("[data-test-id=phone] input").setValue(phone);
         $("[data-test-id=agreement]").click();
         $(".button").click();
@@ -52,10 +59,9 @@ class CardDeliveryOrder {
 
     @Test
     void shouldEnterAnIncorrectCity() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue("Ухта");
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
+        $("[data-test-id='city'] input").setValue(cityEng);
+        $("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(3));
         $("[data-test-id=name] input").setValue(name);
         $("[data-test-id=phone] input").setValue(phone);
         $("[data-test-id=agreement]").click();
@@ -65,10 +71,9 @@ class CardDeliveryOrder {
 
     @Test
     void shouldInterAnIncorrectDate() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue(city);
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(-3));
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").doubleClick().sendKeys(DataGenerator.generateDate(-3));
         $("[data-test-id=name] input").setValue(name);
         $("[data-test-id=phone] input").setValue(phone);
         $("[data-test-id=agreement]").click();
@@ -78,7 +83,6 @@ class CardDeliveryOrder {
 
     @Test
     void shouldLeaveAllFieldsEmpty() {
-        open("http://localhost:9999");
         SelenideElement form = $(".form");
         $(".button").click();
         $("[data-test-id=city]").shouldHave(exactText("Поле обязательно для заполнения"));
